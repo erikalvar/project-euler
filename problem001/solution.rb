ec237a3f53764e24321a02f4098467345df8c808
erikalvar <erikalvarbass@gmail.com>
2020-08-20 21:22:26 -0400

    reworks problem 001

--- problem001/solution.rb
+++ problem001/solution.rb
@@ -2,16 +2,30 @@
 
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
+# def natural_sum(num)
+#   natural_nums = []
+#   index = 3
+#   while index < num
+#     if index % 3 == 0 || index % 5 == 0
+#       natural_nums << index
+#     end
+#     index += 1
+#   end
+#   p natural_nums.sum
+# end
+
+# natural_sum(1000)
+
 def natural_sum(num)
-  natural_nums = []
-  index = 3
+  sum = 0
+  index = 0
   while index < num
     if index % 3 == 0 || index % 5 == 0
-      natural_nums << index
+      sum += index
     end
     index += 1
   end
-  p natural_nums.sum
+  p sum
 end
 
 natural_sum(1000)
